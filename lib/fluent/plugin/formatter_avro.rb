60e12d0b8d599d8a44966b7aafeadeb6f6623c92
ramsateesh <ramsateesh@users.noreply.github.com>
2019-07-16 22:56:34 +1000

    Adding Schema Registry Options

--- lib/fluent/plugin/formatter_avro.rb
+++ lib/fluent/plugin/formatter_avro.rb
@@ -1,5 +1,7 @@
 require 'avro'
 require 'fluent/plugin/formatter'
+require 'net/http'
+require 'json'
 
 module Fluent
   module Plugin
@@ -8,15 +10,38 @@ module Fluent
 
       config_param :schema_file, :string, :default => nil
       config_param :schema_json, :string, :default => nil
+      config_param :schema_uri, :string, :default => nil
+      config_param :schema_id, :string, :default => nil
+      config_param :use_ssl, :bool, :default => false
 
       def configure(conf)
         super
-        if not (@schema_json.nil? ^ @schema_file.nil?) then
-          raise Fluent::ConfigError, 'schema_json or schema_file (but not both) is required'
+        if not (@schema_json.nil? ^ @schema_file.nil? ^ @schema_uri.nil?) then
+          raise Fluent::ConfigError, 'schema_json or schema_file or schema_uri (but not all) is required'
         end
-        if @schema_json.nil? then
+          
+        if not @schema_uri.nil? then
+          if not @schema_id then
+            raise Fluent::ConfigError, 'schema_id is required while using schema_uri'
+          end
+            
+          net = Net::HTTP(@schema_uri)
+          if @use_ssl then
+            net.use_ssl = true
+          end
+            
+          request = Net::HTTP::Get.new("/schemas/ids/" + @schema_id)
+          request["Accept"] = "application/vnd.schemaregistry.v1+json, application/vnd.schemaregistry+json, application/json"
+          response = net.request(request)
+            
+          if response.code == "200" then
+            @schema_json = JSON.parse(response.body)["schema"]
+          end
+            
+        elsif not @schema_file.nil? then
           @schema_json = File.read(@schema_file)
         end
+        
         @schema = Avro::Schema.parse(@schema_json)
         @writer = Avro::IO::DatumWriter.new(@schema)
       end
